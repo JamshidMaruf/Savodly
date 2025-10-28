34ed55377a366a650586c0e01e43861b7d4b75cf
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-10-28 08:33:20 +0500

    test

--- src/Savodly.WebApi/Program.cs
+++ src/Savodly.WebApi/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -13,7 +13,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+   
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
