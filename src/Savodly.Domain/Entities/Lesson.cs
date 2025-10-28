5c1d63335247b4f198a87babaede3358bbc75989
dilmurod-jabborov <futboluz825@gmail.com>
2025-10-28 12:13:19 +0500

    add all entites

--- src/Savodly.Domain/Entities/Lesson.cs
+++ src/Savodly.Domain/Entities/Lesson.cs
@@ -5,7 +5,6 @@ public class Lesson : Auditable
     public int CourseId { get; set; }
     public string Name { get; set; }
     public int ModuleId { get; set; }
-
     public Course Course { get; set; }
     public Module Module { get; set; }
     public ICollection<LessonVideo> LessonsVideos { get; set; }
