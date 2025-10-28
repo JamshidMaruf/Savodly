b6b54c4244ddb608642c6fd202d10525e56620a6
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-10-28 12:14:36 +0500

    test

--- src/Savodly.WebApi/Program.cs
+++ src/Savodly.WebApi/Program.cs
@@ -3,7 +3,6 @@
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
