1ba87161998ee166211b8cced379a43708255941
Shamshodbek <shoyimovshamshodbek855@gmail.com>
2025-10-29 15:02:24 +0500

    Add new coursemodels service

---
+++ src/Savodly.Service/Services/CourseModules/ICourseModulesService.cs
@@ -0,0 +1,11 @@
+﻿using Savodly.Service.Services.CourseModules.Models;
+
+namespace Savodly.Service.Services.CourseModules;
+public interface ICourseModulesServcie
+{
+    Task CreateAsync(ModuleCreateModel model);
+    Task UpdateAsync(int id, ModuleUpdateModel model);
+    Task DeleteAsync(int id);
+    Task<ModuleViewModel> GetByIdAsync(int id);
+    Task<List<ModuleViewModel>> GetAllByCourseIdAsync(int courseId);
+}
